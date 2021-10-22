aad8fd42c93f03baa23c7fbdd0d1500a8dfb7757
dongjay00 <cjswodlehdgn@gmail.com>
2021-10-22 18:40:00 +0900

    package and DB settings

---
+++ src/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const sqlite3 = require("sqlite3").verbose();
+const articleRoute = require("./routes/articles");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const models = require("./models");
+
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+models.sequelize
+  .sync()
+  .then(() => {
+    console.log("Connected to db!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.use("/api/articles", articleRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server is running!");
+});
---
+++ src/models/index.js
@@ -0,0 +1,42 @@
+const fs = require("fs");
+const path = require("path");
+const Sequelize = require("sequelize");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const sequelize = new Sequelize(
+  process.env.DATABASE,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "sqlite",
+    storage: "./data/database.sqlite",
+  }
+);
+
+let db = {};
+
+fs.readdirSync(__dirname)
+  .filter((file) => {
+    return file.indexOf(".") !== 0 && file !== "index.js";
+  })
+  .forEach((file) => {
+    const model = require(path.join(__dirname, "models", file)).default(
+      sequelize,
+      Sequelize
+    );
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach((modelName) => {
+  if ("associate" in db[modelName]) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
